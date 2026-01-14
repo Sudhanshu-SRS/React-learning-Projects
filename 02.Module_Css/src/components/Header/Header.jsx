e48a1a018c2836de8fa17a2a0e83cc3158b84883
Sudhanshu-SRS <sudhanshusakhare808@gmail.com>
2026-01-14 22:37:45 +0530

    feat(learning): understand css modules and use cases

---
+++ 02.Module_Css/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Header.module.css'    
+const Header = () => {
+  return (
+    <div><h1 className={styles.h1}>Header From Class CSS Module</h1>
+    </div>
+  )
+}
+
+export default Header
