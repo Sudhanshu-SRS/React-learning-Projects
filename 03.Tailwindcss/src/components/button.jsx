212a40a59245f118a746e1ab73d0d4af0249f7a3
Sudhanshu-SRS <sudhanshusakhare808@gmail.com>
2026-01-14 23:40:40 +0530

    feat(learning):Understand the Follow of Installing Tailwind & Working with it

---
+++ 03.Tailwindcss/src/components/button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Button() {
+  return (
+    <div className="bg-blue-600">
+      <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">Hellow </button>
+    </div>
+  );
+}
+
+export default Button;
