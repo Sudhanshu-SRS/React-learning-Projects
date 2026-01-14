1a9861c2d24662311770b56962649deb338db5e6
Sudhanshu-SRS <sudhanshusakhare808@gmail.com>
2026-01-15 00:31:41 +0530

    feat(design-ui):Using hybrid file structure and added the ui with two section and a navbar

---
+++ 04.Ui-Project/src/components/Section_1/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between px-4 py-8">
+      <h4 className="bg-black text-white px-4 py-2 uppercase rounded-full">Target Auidance</h4>
+      <button className="bg-gray-200 px-4 py-2 rounded-full uppercase tracking-widest text-sm">Digital Banking Platform</button>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ 04.Ui-Project/src/components/Section_1/Section1.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Center from "./Center";
+
+const Section1 = () => {
+  return (
+    <div className="h-screen bg-amber-100">
+      <Navbar />
+      <Center />
+    </div>
+  );
+};
+
+export default Section1;
