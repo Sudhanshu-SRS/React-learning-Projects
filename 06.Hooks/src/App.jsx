8ac0cbfa97c9af68d7ccefa6f769e4205acc4a67
Sudhanshu-SRS <sudhanshusakhare808@gmail.com>
2026-01-15 16:33:37 +0530

    feat(learned):how to use useState

--- 06.Hooks/src/App.jsx
+++ 06.Hooks/src/App.jsx
@@ -1,34 +1,31 @@
+
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [number, setnumber] = useState(0)
+
+  function increase(){
+    setnumber(number+1)
+  }
+   
+  function decrease(){
+    setnumber(number-1)
+  }
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='parent'>
+          <div className='display'>
+            <h1>{number}</h1>
+          </div>
+          <div className='btn'>
+            <button onWheel={increase}>increase</button>
+            <button onWheel={decrease}>Decrease</button>
+          </div>
+    </div>
   )
 }
 
