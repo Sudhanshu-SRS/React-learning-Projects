c50d4d976a7110b7d9878008167cd568002bfe7f
Sudhanshu-SRS <sudhanshusakhare808@gmail.com>
2026-01-15 18:17:07 +0530

    feat(learned): Two Way Bindinf & Value Props in input

--- 09.twoway_binding/src/App.jsx
+++ 09.twoway_binding/src/App.jsx
@@ -1,16 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Title, setTitle] = useState("");
+
+  function handelechange(e) {
+    setTitle(e.target.value);
+  }
 
   return (
     <div>
-      
+      <form
+        className="form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log("form submited by ", Title);
+          setTitle("");
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter Your Name"
+          value={Title}
+          onChange={(e) => {
+            handelechange(e);
+          }}
+        />
+        <button>submit</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
