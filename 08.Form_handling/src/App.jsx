392d9b46d571a2c77e5f43ddb3a0b240b3a2d91d
Sudhanshu-SRS <sudhanshusakhare808@gmail.com>
2026-01-15 17:40:25 +0530

    feat(learned):how To Prevent Default Of Form

--- 08.Form_handling/src/App.jsx
+++ 08.Form_handling/src/App.jsx
@@ -4,31 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
+ const handelsubmit=(e)=>{
+  e.preventDefault();
+  console.log("form submitted");
+ }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  
+   
+   <div>
+    <form onSubmit={function(e){
+        handelsubmit(e)
+      }}>
+      <label >Name:</label>
+      <input type="text" name="name" placeholder='Enter Your Name' />
+      <br />
+      <button >submit</button>
+    </form>
+   </div>
   )
 }
 
