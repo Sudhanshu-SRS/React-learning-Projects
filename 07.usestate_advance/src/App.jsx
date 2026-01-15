5d257093d12ea34195ddacce811a4e7b4ae48ea7
Sudhanshu-SRS <sudhanshusakhare808@gmail.com>
2026-01-15 17:19:29 +0530

    feat(learned):learned the use of UseState With Object using Destructuring

---
+++ 07.usestate_advance/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import ArrayuseState from './components/Array/ArrayuseState'
+import ObjectuseState from './components/Object/ObjectuseState'
+
+function App() {
+
+
+  return (
+    <div>
+      <ArrayuseState/>
+      <ObjectuseState/>
+    </div>
+  )
+}
+
+export default App
---
+++ 07.usestate_advance/src/components/Object/ObjectuseState.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+const ObjectuseState = () => {
+  
+  
+  const [UserO, setUserO] = useState({names:"sudhanshu",age:24,city:"delhi"})
+  
+  function valuech(){
+    const newobj={...UserO}
+    newobj.age=23;
+    newobj.names="john";
+    setUserO(newobj)
+  }
+  
+  
+  
+  
+  return (
+    <div>
+      <h1>The Value Is {UserO.names}, {UserO.age}, {UserO.city}</h1>
+      <button onClick={valuech}>To change Value By destructuring an object So It Can Have different Reference </button>
+    </div>
+  )
+}
+
+export default ObjectuseState
