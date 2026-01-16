e1c05eae1b569ea87ce336b4ce542842c0f788a2
Sudhanshu-SRS <sudhanshusakhare808@gmail.com>
2026-01-16 23:44:47 +0530

    feat(learned) : how to use fetch and async await in app and usestate

--- 14.Gallery_app/src/App.jsx
+++ 14.Gallery_app/src/App.jsx
@@ -1,45 +1,56 @@
-import { useEffect, useEffectEvent, useState } from "react";
-import "./App.css";
 import axios from "axios";
-function App() {
+import { useState } from "react";
 
-const [userdata, setUserdata] = useState([])
- 
+const App = () => {
+  const [userdata, setUserdata] = useState([]);
 
- const getdata=async ()=>{
-  const response=await axios.get('https://picsum.photos/v2/list?page=2&limit=30')
-  setUserdata(response.data);
- }
- 
- 
+  const getdata = async () => {
+    const response = await axios.get(
+      "https://picsum.photos/v2/list?page=2&limit=30"
+    );
+    setUserdata(response.data);
+  };
 
-
-
+  let userdetail = "No User Data";
+  if (userdata.length > 0) {
+    userdetail = userdata.map((val, id) => {
+      return (
+        
+       <div key={id}>
+          <a href={val.url} target="_blank">
+           <div className="flex flex-col text-center">
+            <div className="w-40 h-48 rounded-2xl overflow-hidden">
+              <img
+                className="h-full w-full object-cover"
+                src={val.download_url}
+                alt=""
+              />
+            </div>
+            <h3>{val.author}</h3>
+          </div>
+         </a>
+       </div>
+        
+      );
+    });
+  }
 
   return (
-    <div className="bg-black h-screen text-white overflow-y-auto">
-      <div className="flex flex-col items-start p-5 gap-5 ">
-        <button onClick={getdata} className="bg-green-400 px-4 py-1 rounded-2xl">
+    <div className="bg-black h-screen overflow-auto text-white">
+      <div className="p-5 flex flex-col items-start gap-4 ">
+        <button
+          onClick={getdata}
+          className="bg-amber-300 text-black px-3 py-2 rounded-4xl "
+        >
           Get Image
         </button>
-        <h2>No Image Availiable </h2>
-      </div>
-
-      <div className="flex flex-wrap  gap-4 p-10  ">
-        {userdata.map((ele,id)=>{
-          return <div key={id} className="w-48 text-center">
-          <img
-            src={ele.download_url}
-            alt="nature"
-            className="mx-auto rounded-lg h-48 w-48 object-cover"
-          /> 
-          <h3 className="text-sm font-semibold flex justify-center mb-5">{ele.author}</h3>
+        <div>
+          <div className="flex flex-wrap gap-4  items-center  ">{userdetail}</div>
         </div>
-        })}
-
+        
       </div>
     </div>
   );
-}
+};
 
 export default App;
