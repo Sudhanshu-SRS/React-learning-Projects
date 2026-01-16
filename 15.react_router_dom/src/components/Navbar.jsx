b753e09c94ff34a0d4566abd36e736ff0a75f234
Sudhanshu-SRS <sudhanshusakhare808@gmail.com>
2026-01-17 01:03:29 +0530

    feat(learned): react Router dom & Its Setup And Also How To Make It Into SPA

---
+++ 15.react_router_dom/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+       <div className='nav'>
+        <div><h2>Navbar</h2></div>
+        <div className='links'> <Link to='/'>Home</Link>
+        <Link to='/contact'>Contact</Link>
+        <Link to='/faq'>FAQ</Link>
+        <Link to='/about'>About</Link></div>
+       </div>
+    </div>
+  )
+}
+
+export default Navbar
