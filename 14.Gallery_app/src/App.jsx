3be107130f8ee30676b12e02ff99903f4c34ab31
Sudhanshu-SRS <sudhanshusakhare808@gmail.com>
2026-01-17 00:06:58 +0530

    feat(learned):How to Use Use Effect & Added A dyanymic Button to forwar and backword

--- 14.Gallery_app/src/App.jsx
+++ 14.Gallery_app/src/App.jsx
@@ -1,15 +1,19 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
   const [userdata, setUserdata] = useState([]);
-
+  const [index, setIndex] = useState(1)
   const getdata = async () => {
     const response = await axios.get(
-      "https://picsum.photos/v2/list?page=2&limit=30"
+      `https://picsum.photos/v2/list?page=${index}&limit=30`
     );
     setUserdata(response.data);
   };
+
+ useEffect(()=>{
+  getdata()
+ },[index])
 
   let userdetail = "No User Data";
   if (userdata.length > 0) {
@@ -28,6 +32,7 @@ const App = () => {
               <h3>{val.author}</h3>
             </div>
           </a>
+          
         </div>
       );
     });
@@ -35,18 +40,27 @@ const App = () => {
 
   return (
     <div className="bg-black h-screen overflow-auto text-white">
-      <div className="p-5 flex flex-col items-start gap-4 ">
-        <button
+      
+        {/* <button
           onClick={getdata}
           className="bg-amber-300 text-black px-3 py-2 rounded-4xl "
         >
           Get Image
-        </button>
-        <div className="flex flex-wrap gap-4 ">
-          <div>{userdetail}</div>
+        </button> */}
+        <div >
+          <div className="flex flex-wrap gap-4 p-5">{userdetail}</div>
+
+          <div className="flex justify-center gap-5 mb-5">
+            <button onClick={()=>{if(index>1){setIndex(index-1)}
+              
+            }} className="bg-yellow-300 text-black px-5 py-2 ">perv</button>
+            <button onClick={()=>{
+              setIndex(index+1)
+            }} className="bg-yellow-300 text-black px-5 py-2 ">next</button>
+          </div>
         </div>
       </div>
-    </div>
+    
   );
 };
 
